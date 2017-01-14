03523242115c7dba3dd9064bb54cfa4cdf32a067
gorums <acksecurity@hotmail.com>
2017-01-14 01:46:47 -0500

    Refactor wiot-agent-hd

--- lib/wiot-agent-hd/cli.rb
+++ lib/wiot-agent-hd/cli.rb
@@ -1,11 +1,11 @@
 module WiotAgentHd
   module CLI
     def parse_options
-
+      puts "Parse options WiotAgentHd ..."
     end
 
     def start(opts)
-
+      puts "Start WiotAgentHd ..."
     end
 
   end
